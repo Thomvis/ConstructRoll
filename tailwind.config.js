4cd23f1642d74a96f24ef27e5f0a8ee9db60a29e
Thomas Visser <thomas.visser@gmail.com>
2022-08-21 20:59:05 +0200

    Add landing page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'indigo-dark': '#262A33'
+      }
+    },
+  },
+  plugins: [],
+}
